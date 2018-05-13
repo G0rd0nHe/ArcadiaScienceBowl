27c3d897eafd5d92195c64b48c146ea068957e7b
G0rd0nHe <39225043+G0rd0nHe@users.noreply.github.com>
2018-05-12 17:31:10 -0700

    Update asteroidCode.js

--- asteroidCode.js
+++ asteroidCode.js
@@ -279,3 +279,10 @@ function keyPressed() {
     game = true;
   }
 }
+
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
